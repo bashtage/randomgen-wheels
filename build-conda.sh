3f416efac059b674ef8022b039c1c8cd016275b4
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-04-15 15:41:52 +0100

    RLD: Build for conda and test.pypi.org

--- build-conda.sh
+++ build-conda.sh
@@ -2,28 +2,30 @@
 
 echo "Inside build-conda.sh"
 
-if [[ ${PLAT} == "x86_64" && ${UNICODE_WIDTH} == "32" && ${CONDA_BUILD} == true ]] || [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
-    if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda3.sh -nv;
+if [[ ${CONDA_BUILD} == true ]]; then
+    if [[ ${PLAT} == "x86_64" && ${UNICODE_WIDTH} == "32" ]] || [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+        if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda3.sh -nv;
+        fi
+        if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+            wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3.sh -nv;
+        fi
+        chmod +x miniconda3.sh
+        ./miniconda3.sh -b
+        export PATH=${HOME}/miniconda3/bin:$PATH
+        conda config --set always_yes true
+        conda update --all --quiet
+        conda install conda-build anaconda-client conda-verify
+        echo cd ${TRAVIS_BUILD_DIR}
+        cd ${TRAVIS_BUILD_DIR}
+        if [[ ${CONDA_UPLOAD} == true ]]; then
+            conda config --set anaconda_upload yes
+        else
+            conda config --set anaconda_upload no
+        fi
+        echo conda build --python ${MB_PYTHON_VERSION} --numpy ${CONDA_NUMPY_VERSION} ./conda-recipe
+        conda build --python ${MB_PYTHON_VERSION} --numpy ${CONDA_NUMPY_VERSION} --user ${ANACONDA_USERNAME} --token ${ANACONDA_TOKEN} ./conda-recipe
     fi
-    if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-        wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3.sh -nv;
-    fi
-    chmod +x miniconda3.sh
-    ./miniconda3.sh -b
-    export PATH=${HOME}/miniconda3/bin:$PATH
-    conda config --set always_yes true
-    conda update --all --quiet
-    conda install conda-build anaconda-client conda-verify
-    echo cd ${TRAVIS_BUILD_DIR}
-    cd ${TRAVIS_BUILD_DIR}
-    if [[ ${CONDA_UPLOAD} == true ]]; then
-        conda config --set anaconda_upload yes
-    else
-        conda config --set anaconda_upload no
-    fi
-    echo conda build --python ${MB_PYTHON_VERSION} --numpy ${CONDA_NUMPY_VERSION} ./conda-recipe
-    conda build --python ${MB_PYTHON_VERSION} --numpy ${CONDA_NUMPY_VERSION} --user ${ANACONDA_USERNAME} --token ${ANACONDA_TOKEN} ./conda-recipe
 else
     echo "conda build is disabled"
 fi
